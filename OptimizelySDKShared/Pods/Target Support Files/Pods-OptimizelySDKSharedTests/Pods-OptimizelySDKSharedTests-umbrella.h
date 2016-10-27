5eb4adfaf9a021e9da1a6b1082882a8ede86cdae
Joshua Wang <josh.wang@optimizely.com>
2016-10-27 13:33:21 -0700

    start shared SDK project (#10)
    Summary: start shared SDK library with Cocoapods depending on JSONModel, OHHTTPStubs, OCMock
    
    Test Plan: check that the project compiles
    
    Reviewers: alda
    
    JIRA Issues: OASIS-265
    
    Differential Revision: https://phabricator.optimizely.com/D13559

---
+++ OptimizelySDKShared/Pods/Target Support Files/Pods-OptimizelySDKSharedTests/Pods-OptimizelySDKSharedTests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_OptimizelySDKSharedTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_OptimizelySDKSharedTestsVersionString[];
+
