5b51b114ab577f8e28529a620512d793dd370e43
wangjoshuah <wangjoshuah@gmail.com>
2016-10-27 14:07:23 -0700

    start OptimizelyManager scaffold

--- OptimizelySDKShared/OptimizelySDKShared/OptimizelyManager.h
+++ OptimizelySDKShared/OptimizelySDKShared/OptimizelyManager.h
@@ -14,14 +14,8 @@
  * limitations under the License.                                           *
  ***************************************************************************/
 
-#import "OptimizelyManager.h"
+#import <Foundation/Foundation.h>
 
-//! Project version number for OptimizelySDKShared.
-FOUNDATION_EXPORT double OptimizelySDKSharedVersionNumber;
+@interface OptimizelyManager : NSObject
 
-//! Project version string for OptimizelySDKShared.
-FOUNDATION_EXPORT const unsigned char OptimizelySDKSharedVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <OptimizelySDKShared/PublicHeader.h>
-
-
+@end
