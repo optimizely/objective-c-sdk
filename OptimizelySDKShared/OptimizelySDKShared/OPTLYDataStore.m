66f619d3a898ed017eb53d7b8e51913faf268779
Alda Luong <alda@optimizely.com>
2016-10-31 00:02:10 -0700

    Added data store scaffold.

---
+++ OptimizelySDKShared/OptimizelySDKShared/OPTLYDataStore.m
@@ -0,0 +1,13 @@
+//
+//  OPTLYDataStore.m
+//  OptimizelySDKShared
+//
+//  Created by Alda Luong on 10/31/16.
+//  Copyright © 2016 Optimizely. All rights reserved.
+//
+
+#import "OPTLYDataStore.h"
+
+@implementation OPTLYDataStore
+
+@end
