325a05c9efda4fa79652a466f3bcbea3a68f895f
Alda Luong <alda@optimizely.com>
2016-10-31 00:01:14 -0700

    Added pods.

---
+++ OptimizelySDKCore/Pods/Target Support Files/Pods-OptimizelySDKCoreTVOS/Pods-OptimizelySDKCoreTVOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_OptimizelySDKCoreTVOS : NSObject
+@end
+@implementation PodsDummy_Pods_OptimizelySDKCoreTVOS
+@end
---
+++ OptimizelySDKCore/Pods/Target Support Files/Pods-OptimizelySDKCoreiOSTests/Pods-OptimizelySDKCoreiOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_OptimizelySDKCoreiOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_OptimizelySDKCoreiOSTests
+@end
