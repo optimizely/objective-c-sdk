41d7f2e35ef5747e964e8b459d4b1cc8717b269b
Alda Luong <alda@optimizely.com>
2016-11-04 15:11:19 -0700

    Removed preview and editor datafile types (put back in when we've implemented them). Fixed some typos. Added more code comments.

--- OptimizelySDKShared/OptimizelySDKSharedTests/OPTLYFileManagerTest.m
+++ OptimizelySDKShared/OptimizelySDKSharedTests/OPTLYFileManagerTest.m
@@ -1,10 +1,18 @@
-//
-//  OPTLYFileManagerTest.m
-//  OptimizelySDKDatafileManager
-//
-//  Created by Alda Luong on 11/3/16.
-//  Copyright © 2016 Optimizely. All rights reserved.
-//
+/****************************************************************************
+ * Copyright 2016, Optimizely, Inc. and contributors                        *
+ *                                                                          *
+ * Licensed under the Apache License, Version 2.0 (the "License");          *
+ * you may not use this file except in compliance with the License.         *
+ * You may obtain a copy of the License at                                  *
+ *                                                                          *
+ *    http://www.apache.org/licenses/LICENSE-2.0                            *
+ *                                                                          *
+ * Unless required by applicable law or agreed to in writing, software      *
+ * distributed under the License is distributed on an "AS IS" BASIS,        *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
+ * See the License for the specific language governing permissions and      *
+ * limitations under the License.                                           *
+ ***************************************************************************/
 
 #import <XCTest/XCTest.h>
 #import "OPTLYFileManager.h"
@@ -130,14 +138,6 @@ static NSString *const kTestString = @"testString";
     [self.fileManager saveFile:kTestFileName
                           data:self.testData
                           type:OPTLYFileManagerDataTypeEventDispatcher
-                         error:nil];
-    [self.fileManager saveFile:kTestFileName
-                          data:self.testData
-                          type:OPTLYFileManagerDataTypePreview
-                         error:nil];
-    [self.fileManager saveFile:kTestFileName
-                          data:self.testData
-                          type:OPTLYFileManagerDataTypeEditor
                          error:nil];
     [self.fileManager removeAllFiles:nil];
     
