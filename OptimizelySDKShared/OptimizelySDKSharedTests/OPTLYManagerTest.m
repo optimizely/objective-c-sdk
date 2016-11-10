e92090c59b403ae793083f0b6ea928563e71ce9e
wangjoshuah <wangjoshuah@gmail.com>
2016-11-10 14:54:13 -0800

    add test for initialization of client

--- OptimizelySDKShared/OptimizelySDKSharedTests/OPTLYManagerTest.m
+++ OptimizelySDKShared/OptimizelySDKSharedTests/OPTLYManagerTest.m
@@ -15,15 +15,19 @@
  ***************************************************************************/
 
 #import <XCTest/XCTest.h>
+#import <OHHTTPStubs/OHHTTPStubs.h>
 #import "OPTLYTestHelper.h"
 
+#import <OptimizelySDKCore/OPTLYProjectConfig.h>
 #import "OPTLYManager.h"
 #import "OPTLYClient.h"
 
 // static datafile name
 static NSString *const kDefaultDatafileFileName = @"datafile_6372300739";
 static NSString *const kProjectId = @"6372300739";
+static NSString *const kAlternateDatafilename = @"validator_whitelisting_test_datafile";
 static NSData *kDefaultDatafile;
+static NSData *kAlternateDatafile;
 
 @interface OPTLYManagerTest : XCTestCase
 
@@ -34,6 +38,126 @@ static NSData *kDefaultDatafile;
 + (void)setUp {
     [super setUp];
     kDefaultDatafile = [OPTLYTestHelper loadJSONDatafileIntoDataObject:kDefaultDatafileFileName];
+    kAlternateDatafile = [OPTLYTestHelper loadJSONDatafileIntoDataObject:kAlternateDatafilename];
 }
+
+- (void)testInitializeClientWithoutDatafileReturnsDummy {
+    // initialize manager without datafile
+    OPTLYManager *manager = [OPTLYManager initWithBuilderBlock:^(OPTLYManagerBuilder * _Nullable builder) {
+        builder.projectId = kProjectId;
+    }];
+    
+    // make sure the manager is initialized correctly
+    XCTAssertNotNil(manager);
+    XCTAssertNotNil(manager.projectId);
+    XCTAssertNil(manager.datafile);
+    
+    // try to initialize client
+    OPTLYClient *client = [manager initializeClient];
+    
+    // make sure we get a dummy client back
+    XCTAssertNotNil(client);
+    XCTAssertNil(client.optimizely);
+    XCTAssertNotNil(client.logger);
+}
+
+- (void)testInitializeClientWithDefaults {
+    // initialize manager
+    OPTLYManager *manager = [OPTLYManager initWithBuilderBlock:^(OPTLYManagerBuilder * _Nullable builder) {
+        builder.datafile = kDefaultDatafile;
+        builder.projectId = kProjectId;
+    }];
+    
+    // make sure manager is initialized correctly
+    XCTAssertNotNil(manager);
+    XCTAssertNotNil(manager.datafile);
+    XCTAssertEqual(manager.datafile, kDefaultDatafile);
+    
+    // initialize client
+    OPTLYClient *client = [manager initializeClient];
+    
+    // test client initialization
+    XCTAssertNotNil(client);
+    XCTAssertNotNil(client.optimizely);
+    XCTAssertNotNil(client.logger);
+    
+    [self checkConfigIsUsingDefaultDatafile:client.optimizely.config];
+}
+
+- (void)testInitializeClientWithCustomDatafile {
+    // initialize manager
+    OPTLYManager *manager = [OPTLYManager initWithBuilderBlock:^(OPTLYManagerBuilder * _Nullable builder) {
+        builder.datafile = kDefaultDatafile;
+        builder.projectId = kProjectId;
+    }];
+    
+    // make sure manager is initialized correctly
+    XCTAssertNotNil(manager);
+    XCTAssertNotNil(manager.datafile);
+    XCTAssertEqual(manager.datafile, kDefaultDatafile);
+    
+    // initialize client
+    OPTLYClient *client = [manager initializeClientWithDatafile:kAlternateDatafile];
+    
+    // test client initialization
+    XCTAssertNotNil(client);
+    XCTAssertNotNil(client.optimizely);
+    XCTAssertNotNil(client.logger);
+    
+    [self checkConfigIsUsingAlternativeDatafile:client.optimizely.config];
+}
+
+- (void)testInitializeClientAsync {
+    // initialize manager
+    OPTLYManager *manager = [OPTLYManager initWithBuilderBlock:^(OPTLYManagerBuilder * _Nullable builder) {
+        builder.datafile = kDefaultDatafile;
+        builder.projectId = kProjectId;
+    }];
+    
+    // make sure manager is initialized correctly
+    XCTAssertNotNil(manager);
+    XCTAssertNotNil(manager.datafile);
+    XCTAssertEqual(manager.datafile, kDefaultDatafile);
+    
+    // stub network call
+    [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
+        return [request.URL.host isEqualToString:@"cdn.optimizely.com"];
+    } withStubResponse:^OHHTTPStubsResponse*(NSURLRequest *request) {
+        // Stub it with our "wsresponse.json" stub file (which is in same bundle as self)
+        return [OHHTTPStubsResponse responseWithData:kAlternateDatafile
+                                          statusCode:200
+                                             headers:@{@"Content-Type":@"application/json"}];
+    }];
+    
+    // setup async expectation
+    __weak XCTestExpectation *expectation = [self expectationWithDescription:@"testInitializeClientAsync"];
+    // initialize client
+    [manager initializeClientWithCallback:^(NSError * _Nullable error, OPTLYClient * _Nullable client) {
+        
+        // check client in callback
+        XCTAssertNotNil(client);
+        XCTAssertNotNil(client.optimizely, @"Client needs to have an optimizely instance");
+        XCTAssertNotNil(client.logger);
+        [self checkConfigIsUsingAlternativeDatafile:client.optimizely.config];
+        [expectation fulfill];
+    }];
+    
+    // wait for async start to finish
+    [self waitForExpectationsWithTimeout:2 handler:nil];
+}
+
+- (void)checkConfigIsUsingDefaultDatafile: (OPTLYProjectConfig *)config {
+    XCTAssertEqualObjects(config.revision, @"58");
+    XCTAssertEqualObjects(config.projectId, @"6372300739");
+    XCTAssertEqualObjects(config.accountId, @"6365361536");
+}
+
+- (void)checkConfigIsUsingAlternativeDatafile: (OPTLYProjectConfig *)config {
+    XCTAssertEqualObjects(config.revision, @"6");
+    XCTAssertEqualObjects(config.projectId, @"7519590183");
+    XCTAssertEqualObjects(config.accountId, @"3244610124");
+}
+
+
 
 @end
