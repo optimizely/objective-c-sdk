365d067c2bb850aa30419aeb7c6568903fc37f9e
wangjoshuah <wangjoshuah@gmail.com>
2016-11-15 15:56:26 -0800

    test save datafile is successful

--- OptimizelySDKDatafileManager/OptimizelySDKDatafileManagerTests/OPTLYDatafileManagerTest.m
+++ OptimizelySDKDatafileManager/OptimizelySDKDatafileManagerTests/OPTLYDatafileManagerTest.m
@@ -7,7 +7,9 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "OPTLYTestHelper.h"
 #import <OHHTTPStubs/OHHTTPStubs.h>
+#import <OptimizelySDKShared/OPTLYFileManager.h>
 
 #import "OPTLYDatafileManager.h"
 
@@ -15,7 +17,21 @@
 
 @end
 
-static NSString *kProjectId = @"projectId";
+@interface OPTLYDatafileManager ()
+
+- (void)saveDatafile:(NSData *)datafile;
+
+@end
+
+@interface OPTLYFileManager ()
+
+@property NSString *baseDir;
+- (NSString *)stringForDataTypeEnum:(OPTLYFileManagerDataType)dataType;
+
+@end
+
+static NSString *const kProjectId = @"6372300739";
+static NSString *const kDatamodelDatafileName = @"datafile_6372300739";
 
 @implementation OPTLYDatafileManagerTest
 
@@ -54,6 +70,36 @@ static NSString *kProjectId = @"projectId";
     
     // clean stubs
     [OHHTTPStubs removeStub:stub];
+}
+
+- (void)testSaveDatafile {
+    // setup datafile manager
+    OPTLYDatafileManager *datafileManager = [OPTLYDatafileManager initWithBuilderBlock:^(OPTLYDatafileManagerBuilder * _Nullable builder) {
+        builder.projectId = kProjectId;
+    }];
+    XCTAssertNotNil(datafileManager);
+    
+    // get the datafile
+    NSData *datafile = [OPTLYTestHelper loadJSONDatafileIntoDataObject:kDatamodelDatafileName];
+    
+    // save the datafile
+    [datafileManager saveDatafile:datafile];
+    
+    // test the datafile was saved correctly
+    NSString *expectedFilePath = [self getExpectedFilePathForProjectId:datafileManager.projectId];
+    NSFileManager *defaultFileManager = [NSFileManager defaultManager];
+    bool fileExists = [defaultFileManager fileExistsAtPath:expectedFilePath];
+    XCTAssertTrue(fileExists, @"datafile was not saved properly");
+    NSData *fileData = [NSData dataWithContentsOfFile:expectedFilePath options:0 error:nil];
+    XCTAssert(fileData != nil, @"Saved file has no content.");
+    XCTAssert([fileData isEqualToData:datafile],  @"Invalid file content of saved file.");
+}
+
+- (NSString *)getExpectedFilePathForProjectId:(NSString *)projectId {
+    OPTLYFileManager *fileManager = [OPTLYFileManager new];
+    NSString *fileDir = [fileManager.baseDir stringByAppendingPathComponent:[fileManager stringForDataTypeEnum:OPTLYFileManagerDataTypeDatafile]];
+    NSString *filePath = [fileDir stringByAppendingPathComponent:[NSString stringWithFormat:@"%@.txt", projectId]];
+    return filePath;
 }
 
 @end
