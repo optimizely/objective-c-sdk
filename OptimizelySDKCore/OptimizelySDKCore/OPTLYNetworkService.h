7e50b3e64c4e181187a6474636f5e306bc7de70a
wangjoshuah <wangjoshuah@gmail.com>
2016-12-21 13:30:23 -0800

    fix nonnull in network service

--- OptimizelySDKCore/OptimizelySDKCore/OPTLYNetworkService.h
+++ OptimizelySDKCore/OptimizelySDKCore/OPTLYNetworkService.h
@@ -58,6 +58,6 @@ NS_ASSUME_NONNULL_END
  * Returns the URL path for the datafile of a particular project.
  * @param projectId The project ID of the datafile whose URL path we are looking for.
  */
-+ (NSURL *)projectConfigURLPath:(NSString *)projectId;
++ (nonnull NSURL *)projectConfigURLPath:(nonnull NSString *)projectId;
 
 @end
