2f6e72c7b29692e8545af9f86040c1cc22658511
Alda Luong <alda@optimizely.com>
2017-03-17 16:17:57 -0700

    Fixed pods issue.

---
+++ OptimizelySDKUniversal/OptimizelySDKUniversal/OptimizelySDKUniversal.h
@@ -0,0 +1,24 @@
+//
+//  OptimizelySDK.h
+//  OptimizelySDK
+//
+//  Created by Alda Luong on 3/16/17.
+//  Copyright © 2017 Optimizely. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "OptimizelySDKCore.h"
+#import "OptimizelySDKDatafileManager.h"
+#import "OptimizelySDKEventDispatcher.h"
+#import "OptimizelySDKShared.h"
+#import "OptimizelySDKUserProfile.h"
+
+//! Project version number for OptimizelySDK.
+FOUNDATION_EXPORT double OptimizelySDKVersionNumber;
+
+//! Project version string for OptimizelySDK.
+FOUNDATION_EXPORT const unsigned char OptimizelySDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OptimizelySDK/PublicHeader.h>
+
+
--- OptimizelySDKiOS/OptimizelySDKiOS/OPTLYManager.m
+++ OptimizelySDKiOS/OptimizelySDKiOS/OPTLYManager.m
@@ -123,7 +123,7 @@ static NSString * const kClientEngine = @"ios-sdk";
         
         // --- client version ---
 #ifdef UNIVERSAL
-        _clientVersion = OPTIMIZELY_SDK_IOS_UNIVERSAL_VERSION;
+//_clientVersion = OPTIMIZELY_SDK_iOS_UNIVERSAL_VERSION;
 #else
         _clientVersion = OPTIMIZELY_SDK_iOS_VERSION;
 #endif
