329e10a14e683e040364cc66bb5d46a40cfb0b8c
Alda Luong <alda@optimizely.com>
2017-06-05 17:40:44 -0700

    Updated the generated frameworks.

---
+++ OptimizelySDKUniversal/generated-frameworks/Release-iOS-universal-SDK/OptimizelySDKiOS.framework/Headers/OPTLYDatafileManager.h
@@ -0,0 +1,47 @@
+/****************************************************************************
+ * Copyright 2016, Optimizely, Inc. and contributors                        *
+ *                                                                          *
+ * Licensed under the Apache License, Version 2.0 (the "License");          *
+ * you may not use this file except in compliance with the License.         *
+ * You may obtain a copy of the License at                                  *
+ *                                                                          *
+ *    http://www.apache.org/licenses/LICENSE-2.0                            *
+ *                                                                          *
+ * Unless required by applicable law or agreed to in writing, software      *
+ * distributed under the License is distributed on an "AS IS" BASIS,        *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
+ * See the License for the specific language governing permissions and      *
+ * limitations under the License.                                           *
+ ***************************************************************************/
+
+#import <Foundation/Foundation.h>
+#ifdef UNIVERSAL
+    #import "OPTLYDatafileManager.h"
+#else
+    #import <OptimizelySDKShared/OPTLYDatafileManagerBasic.h>
+#endif
+#import "OPTLYDatafileManagerBuilder.h"
+
+@protocol OPTLYDatafileManager, OPTLYErrorHandler, OPTLYLogger;
+
+@interface OPTLYDatafileManagerDefault : NSObject<OPTLYDatafileManager>
+
+/// The time interval to regularly fetch the datafile.
+@property (nonatomic, readonly) NSTimeInterval datafileFetchInterval;
+/// This flag specifies if the datafile has been cached or not.
+@property (nonatomic, assign) BOOL isDatafileCached;
+/// The project ID of the datafile this datafile manager will monitor
+@property (nonatomic, readonly, strong, nonnull) NSString *projectId;
+/// The error handler to be used for the manager, client, and all subcomponents
+@property (nonatomic, readonly, strong, nullable) id<OPTLYErrorHandler> errorHandler;
+/// A logger for the OPTLYDatafileManager to log messages.
+@property (nonatomic, readonly, strong, nonnull) id<OPTLYLogger> logger;
+
+/**
+ * Init with builder block
+ * @param builderBlock The builder block containing the datafile fetch interval.
+ * @return an Optimizely Datafile Manager instance.
+ */
++ (nullable instancetype)init:(nonnull OPTLYDatafileManagerBuilderBlock)builderBlock;
+
+@end
---
+++ OptimizelySDKUniversal/generated-frameworks/Release-iOS-universal-SDK/OptimizelySDKiOS.framework/Headers/OPTLYEventBuilder.h
@@ -0,0 +1,72 @@
+/****************************************************************************
+ * Copyright 2016, Optimizely, Inc. and contributors                        *
+ *                                                                          *
+ * Licensed under the Apache License, Version 2.0 (the "License");          *
+ * you may not use this file except in compliance with the License.         *
+ * You may obtain a copy of the License at                                  *
+ *                                                                          *
+ *    http://www.apache.org/licenses/LICENSE-2.0                            *
+ *                                                                          *
+ * Unless required by applicable law or agreed to in writing, software      *
+ * distributed under the License is distributed on an "AS IS" BASIS,        *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
+ * See the License for the specific language governing permissions and      *
+ * limitations under the License.                                           *
+ ***************************************************************************/
+
+/*
+ This class packages the parameters for impression and conversion events sent to the backend.
+ */
+
+#import <Foundation/Foundation.h>
+
+@class OPTLYProjectConfig, OPTLYDecisionEventTicket, OPTLYEventTicket;
+@protocol OPTLYBucketer;
+
+// --- Event URLs ----
+NS_ASSUME_NONNULL_BEGIN
+extern NSString * const OPTLYEventBuilderDecisionTicketEventURL;
+extern NSString * const OPTLYEventBuilderEventTicketURL;
+NS_ASSUME_NONNULL_END
+
+@protocol OPTLYEventBuilder <NSObject>
+
+/**
+ * Create the parameters for an impression event.
+ *
+ * @param config The project config object.
+ * @param userId The ID of the user.
+ * @param experimentKey The experiment name.
+ * @param variationId The variation ID.
+ * @param attributes A map of attribute names to current user attribute values.
+ * @return A map of parameters for an impression event. This value can be nil.
+ *
+ */
+- (nullable NSDictionary *)buildDecisionEventTicket:(nonnull OPTLYProjectConfig *)config
+                                             userId:(nonnull NSString *)userId
+                                      experimentKey:(nonnull NSString *)experimentKey
+                                        variationId:(nonnull NSString *)variationId
+                                         attributes:(nullable NSDictionary<NSString *, NSString *> *)attributes;
+
+/**
+ * Create the parameters for a conversion event.
+ *
+ * @param config The project config object.
+ * @param userId The ID of the user.
+ * @param eventName The event name.
+ * @param eventTags A map of event tag names to event tag values (NSString or NSNumber containing float, double, integer, or boolean).
+ * @param attributes A map of attribute names to current user attribute values.
+ * @return A map of parameters for a conversion event. This value can be nil.
+ *
+ */
+- (nullable NSDictionary *)buildEventTicket:(nonnull OPTLYProjectConfig *)config
+                                   bucketer:(nonnull id<OPTLYBucketer>)bucketer
+                                     userId:(nonnull NSString *)userId
+                                  eventName:(nonnull NSString *)eventName
+                                  eventTags:(nullable NSDictionary *)eventTags
+                                 attributes:(nullable NSDictionary<NSString *, NSString *> *)attributes;
+@end
+
+@interface OPTLYEventBuilderDefault : NSObject<OPTLYEventBuilder>
+
+@end
