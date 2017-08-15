49f78800b247e26fc2cb25ba46634980d37fb5bc
Kelly Roach <kelly.roach@optimizely.com>
2017-08-15 14:01:51 -0700

    Add empty FMDBTests target to OptimizelySDKShared project.

---
+++ OptimizelySDKShared/FMDBTests/FMDBTests.m
@@ -0,0 +1,39 @@
+//
+//  FMDBTests.m
+//  FMDBTests
+//
+//  Created by Kelly Roach on 8/15/17.
+//  Copyright © 2017 Optimizely. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FMDBTests : XCTestCase
+
+@end
+
+@implementation FMDBTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
