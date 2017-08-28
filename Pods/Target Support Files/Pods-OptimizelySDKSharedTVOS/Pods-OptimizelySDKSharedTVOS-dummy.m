fa26a689ff1aac673bcc322c4fe18a3b48bdfd8e
Kelly Roach <kelly.roach@optimizely.com>
2017-08-28 10:49:03 -0700

    pod install

--- Pods/Target Support Files/Pods-OptimizelySDKSharedTVOS/Pods-OptimizelySDKSharedTVOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_OptimizelySDKSharedTVOS : NSObject
-@end
-@implementation PodsDummy_Pods_OptimizelySDKSharedTVOS
-@end
--- Pods/Target Support Files/Pods-OptimizelySDKUserProfileServiceTVOS/Pods-OptimizelySDKUserProfileServiceTVOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_OptimizelySDKUserProfileServiceTVOS : NSObject
-@end
-@implementation PodsDummy_Pods_OptimizelySDKUserProfileServiceTVOS
-@end
