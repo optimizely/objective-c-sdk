cb98638b4e0386415bf350579d271607d05c6eb8
Thomas Zurkan <thomas.zurkan@optimizely.com>
2018-06-12 21:29:13 -0700

    update headers

--- OptimizelySDKShared/OptimizelySDKShared/OPTLYDatafileConfig.m
+++ OptimizelySDKShared/OptimizelySDKShared/OPTLYDatafileConfig.m
@@ -1,13 +1,23 @@
-//
-//  DatafileConfig.m
-//  OptimizelySDKShared
-//
-//  Created by Thomas Zurkan on 6/11/18.
-//  Copyright © 2018 Optimizely. All rights reserved.
-//
+/****************************************************************************
+ * Copyright 2018, Optimizely, Inc. and contributors                        *
+ *                                                                          *
+ * Licensed under the Apache License, Version 2.0 (the "License");          *
+ * you may not use this file except in compliance with the License.         *
+ * You may obtain a copy of the License at                                  *
+ *                                                                          *
+ *    http://www.apache.org/licenses/LICENSE-2.0                            *
+ *                                                                          *
+ * Unless required by applicable law or agreed to in writing, software      *
+ * distributed under the License is distributed on an "AS IS" BASIS,        *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
+ * See the License for the specific language governing permissions and      *
+ * limitations under the License.                                           *
+ ***************************************************************************/
 
 #import "OPTLYDatafileConfig.h"
 #import "NSString+OPTLYCategory.h"
+
+NSString * const DATAFILE_URL = @"https://cdn.optimizely.com/json/%@.json";
 
 @interface OPTLYDatafileConfig()
 @property(nonatomic, strong, nullable) NSString* projectId;
@@ -33,7 +43,7 @@
 }
 
 - (NSURL *)URLForKey {
-    NSString *filePath = [NSString stringWithFormat:@"https://cdn.optimizely.com/json/%@.json", [self key]];
+    NSString *filePath = [NSString stringWithFormat:DATAFILE_URL, [self key]];
     return [NSURL URLWithString:filePath];
 }
 
