925b3d60dced02677dee43074f16b00b9256d581
Abdur Rafay <arafay@folio3.com>
2018-06-21 16:17:56 +0500

    Copyright headers updated

--- OptimizelySDKShared/OPTLYFMDB/src/optlyfmdb/OPTLYFMDBDatabase.h
+++ OptimizelySDKShared/OPTLYFMDB/src/optlyfmdb/OPTLYFMDBDatabase.h
@@ -1,6 +1,6 @@
 /****************************************************************************
  * Modifications to FMDB by Optimizely, Inc.                                *
- * Copyright 2017, Optimizely, Inc. and contributors                        *
+ * Copyright 2018, Optimizely, Inc. and contributors                        *
  *                                                                          *
  * Licensed under the Apache License, Version 2.0 (the "License");          *
  * you may not use this file except in compliance with the License.         *
