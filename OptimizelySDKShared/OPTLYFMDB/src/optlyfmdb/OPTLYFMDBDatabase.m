9a5922f505edf1f9f4c412fd0ae69917408564c8
AbdurRafay <arafay@folio3.com>
2018-06-23 05:17:09 +0500

    Changed enum type SqliteValueType to OPTLYSqliteValueType (#271)
    * changed enum type SqliteValueType to OPTLYSqliteValueType
    
    * Copyright headers updated

--- OptimizelySDKShared/OPTLYFMDB/src/optlyfmdb/OPTLYFMDBDatabase.m
+++ OptimizelySDKShared/OPTLYFMDB/src/optlyfmdb/OPTLYFMDBDatabase.m
@@ -1,6 +1,6 @@
 /****************************************************************************
  * Modifications to FMDB by Optimizely, Inc.                                *
- * Copyright 2017, Optimizely, Inc. and contributors                        *
+ * Copyright 2018, Optimizely, Inc. and contributors                        *
  *                                                                          *
  * Licensed under the Apache License, Version 2.0 (the "License");          *
  * you may not use this file except in compliance with the License.         *
@@ -1502,7 +1502,7 @@ void OPTLYFMDBBlockSQLiteCallBackFunction(sqlite3_context *context, int argc, sq
 #endif
 }
 
-- (SqliteValueType)valueType:(void *)value {
+- (OPTLYSqliteValueType)valueType:(void *)value {
     return sqlite3_value_type(value);
 }
 
