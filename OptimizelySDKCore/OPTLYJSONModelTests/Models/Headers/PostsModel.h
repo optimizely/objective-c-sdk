971eae47edc7b31e6cbe0b0c6f94d62727f872f4
Yasir Ali <>
2018-11-09 09:01:36 +0500

    Updates license

--- OptimizelySDKCore/OPTLYJSONModelTests/Models/Headers/PostsModel.h
+++ OptimizelySDKCore/OPTLYJSONModelTests/Models/Headers/PostsModel.h
@@ -7,7 +7,7 @@
 //
 /****************************************************************************
  * Modifications to JSONModel by Optimizely, Inc.                           *
- * Copyright 2017, Optimizely, Inc. and contributors                        *
+ * Copyright 2017-2018, Optimizely, Inc. and contributors                        *
  *                                                                          *
  * Licensed under the Apache License, Version 2.0 (the "License");          *
  * you may not use this file except in compliance with the License.         *
