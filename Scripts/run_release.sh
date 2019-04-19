b7ab8161d1bf90afc190724c00492a0f56468560
Juan Carlos Tong <juancarlos.tong@optimizely.com>
2019-04-18 18:37:48 -0700

    automate github release

--- Scripts/run_release.sh
+++ Scripts/run_release.sh
@@ -1,12 +1,24 @@
 #!/usr/bin/env bash
-
 set -e
 
-MYREPO=${HOME}/workdir/${TRAVIS_REPO_SLUG}
-rm -rf ${MYREPO}
-mkdir -p ${MYREPO}
-git clone -b ${TRAVIS_BRANCH} https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG} ${MYREPO}
-cd ${MYREPO}
-git tag "v${VERSION}"
-#git push --tags
-echo "do cocoapods stuff"
+function release_github {
+  CHANGELOG="CHANGELOG.md"
+
+  NEW_VERSION=$(grep '^## ' ${CHANGELOG} | awk 'NR==1')
+  LAST_VERSION=$(grep '^## ' ${CHANGELOG} | awk 'NR==2')
+
+  DESCRIPTION=$(awk "/^${NEW_VERSION}$/,/^${LAST_VERSION}$/" ${CHANGELOG} | grep -v "^${LAST_VERSION}$")
+
+  hub release create v${VERSION} -m "Release ${VERSION}" -m "${DESCRIPTION}"
+}
+
+function release_cocoapods {
+  echo "do cocoapods"
+}
+
+function main {
+  release_github
+  release_cocoapods
+}
+
+main
