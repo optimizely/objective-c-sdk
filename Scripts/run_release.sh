ca0bfa9a1322c0a779c5eb02c58a1734881286c5
Juan Carlos Tong <juancarlos.tong@optimizely.com>
2019-04-25 10:20:29 -0700

    pod trunk push steps

--- Scripts/run_release.sh
+++ Scripts/run_release.sh
@@ -5,10 +5,13 @@ set -e
 # GITHUB_TOKEN - github api token with repo permissions (display value in build log setting: OFF)
 # GITHUB_USER - github username that GITHUB_TOKEN is associated with (display value in build log setting: ON)
 
+# COCOAPODS_TRUNK_TOKEN - should be defined in job settings so that we can `pod trunk push`
+
 # GITHUB_RELEASE_DRAFT - set to true for testing. creates a github release draft instead of publishing release.
 # defaults to false (ie. publishes release for real)
 
 GITHUB_RELEASE_DRAFT=${GITHUB_RELEASE_DRAFT:-false}
+
 
 function release_github {
   CHANGELOG="CHANGELOG.md"
@@ -26,12 +29,27 @@ function release_github {
 }
 
 function release_cocoapods {
-  echo "do cocoapods"
+
+  # ---- Optimizely's pods ----
+  pods=(OptimizelySDKCore OptimizelySDKShared OptimizelySDKDatafileManager OptimizelySDKEventDispatcher OptimizelySDKUserProfileService OptimizelySDKiOS OptimizelySDKTVOS);
+  number_pods=${#pods[@]};
+
+  # ---- push podspecs to cocoapods ----
+  # The podspecs need to be pushed in the correct order because of dependencies!
+  printf "\n\n11. Pushing podspecs to COCOAPODS.ORG .\n";
+  for (( i = 0; i < ${number_pods}; i++ ));
+  do
+    podname=${pods[i]};
+    printf "Pushing the ${podname} pod to COCOAPODS.ORG .\n"
+    pod trunk push --allow-warnings ${podname}.podspec
+    pod update
+  done
+
 }
 
 function main {
   release_github
-  release_cocoapods
+  #release_cocoapods
 }
 
 main
