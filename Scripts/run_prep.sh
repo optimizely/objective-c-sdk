0e8a2c0cf92c489f3e79005a4632fd2d95b9cca9
JC <40373238+juancarlostong@users.noreply.github.com>
2019-06-11 14:09:00 -0700

    ci(travis): fix order of run_prep.sh (#420)

--- Scripts/run_prep.sh
+++ Scripts/run_prep.sh
@@ -43,7 +43,7 @@ function do_stuff {
   trap "kill $!" EXIT
   trap 'error_handler' ERR
 
-  myscripts=( "build_all.sh" "unexported_symbols/unexported_symbols.sh" "test_all.sh" "update_version.sh ${VERSION}" )
+  myscripts=( "update_version.sh ${VERSION}" "build_all.sh" "unexported_symbols/unexported_symbols.sh" "test_all.sh" )
   for i in "${myscripts[@]}"; do
     echo -n "${i} "
     Scripts/${i} >> $BUILD_OUTPUT 2>&1
